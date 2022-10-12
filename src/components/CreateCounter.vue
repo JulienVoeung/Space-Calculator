acd79848fb14e094e69bff4442b1db9f08ddc725
Tanous Raad <raad@et.esiea.fr>
2022-10-13 01:34:59 +0200

    Added counter import & counter subscription

--- src/components/CreateCounter.vue
+++ src/components/CreateCounter.vue
@@ -1,7 +1,32 @@
 <script setup>
 import { defineComponent } from "vue";
+import { state } from "src/stores/countersState";
+import useSupabase from "src/boot/supabase";
+
 
 defineComponent({ name: "CreateCounter" });
+
+const { supabase } = useSupabase();
+
+const pullFromCode = async (code) => {
+    const { data, error } = await supabase
+    .from('counters')
+    .select('value')
+    .match({counter_id: code});
+    if (error) throw error;
+    state.setVal('A', data[0].value);
+  }
+
+function subscribe() {
+  console.log('Sub');
+  const counters = supabase
+  .from('counters')
+  .on('*', payload => {
+    state.setVal('A', payload.new.value);
+    console.log('Subscribe event: Counter updated.')
+  })
+  .subscribe();
+}
 </script>
 
 <template lang="pug">
@@ -28,7 +53,7 @@ defineComponent({ name: "CreateCounter" });
         size="0.9em",
         icon="sync",
         label="Pull shared counters",
-        @click = "syncFromServer"
+        @click = "pullFromCode('d841d966-238b-4acc-8d81-cf3d592e8c89'); subscribe()"
         )
         q-tooltip(anchor="bottom right").bg-teal Pull shared counters
 </template>
