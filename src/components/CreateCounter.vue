399c5b835ddb8ac828cca2ba12fff4117e717269
JulienVoeung <voeung.julien@gmail.com>
2022-10-12 14:51:44 +0200

    commit du create et pull

---
+++ src/components/CreateCounter.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineComponent } from "vue";
+
+defineComponent({ name: "CreateCounter" });
+</script>
+
+<template lang="pug">
+.column.justify-evenly
+  .row.justify-center
+    .column.col-4
+      q-btn.q-ma-xs.bg-teal-13(
+        rounded,
+        dense,
+        no-caps,
+        size="0.9em",
+        icon="add",
+        label="Create a new counter",
+        @click = "syncToServer"
+        )
+        q-tooltip(anchor="bottom left").bg-teal update server values
+    .column.col-5
+      q-btn.q-ma-xs.bg-teal-13(
+        rounded,
+        dense,
+        no-caps,
+        size="0.9em",
+        icon="sync",
+        label="Pull shared counters",
+        @click = "syncFromServer"
+        )
+        q-tooltip(anchor="bottom right").bg-teal update local values from server
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { defineComponent, ref } from 'vue'
-import CounterComponent from '../components/CounterComponent.vue'
-import CountersTotal from '../components/CountersTotal.vue'
-import useAuthUser from 'src/composables/UseAuthUser'
+import CounterComponent from '../components/CounterComponent.vue';
+import CountersTotal from '../components/CountersTotal.vue';
+import useAuthUser from 'src/composables/UseAuthUser';
+import CreateCounter from 'src/components/CreateCounter.vue';
 
 defineComponent({
   name: 'IndexPage'
@@ -14,13 +15,13 @@ const { user } = useAuthUser()
 <template lang="pug">
 q-page.column
   div(v-if="user")
-    div(v-for="n in 5")
-      .row.justify-center.items-start.q-my-xl
-        span.row.flex.flex-center
-          CounterComponent(id="A")
-        span.row.justify-center.items-start
-          span.text-h4.text-purple-9.q-my-md   =
-          CountersTotal
+    CreateCounter
+    .row.justify-center.items-start.q-my-xl
+      span.row.flex.flex-center
+        CounterComponent(id="A")
+      span.row.justify-center.items-start
+        span.text-h4.text-purple-9.q-my-md   =
+        CountersTotal
   div(v-else)
     h4.flex.flex-center.text-deep-purple-7.q-mx-xl You are not logged in !
 </template>
