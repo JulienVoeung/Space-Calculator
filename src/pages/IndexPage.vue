e62fe5dab3dbc9840249de7280d3d9296e3a0bc7
Lionel RAKOTOARISOA <rakotoarisoa@et.esiea.fr>
2022-10-12 14:40:22 +0200

    deleted sign up while connected

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,18 +13,14 @@ const { user } = useAuthUser()
 
 <template lang="pug">
 q-page.column
-  //p(v-if="!!user").q-ma-lg.text-h6.text-purple-9.flex.flex-center Hello {{ user.user_metadata.name }}
-  //p.q-ma-lg Hello {{ user }}
   div(v-if="user")
-    .row.justify-center.items-start.q-my-xl
-      span.row.flex.flex-center
-        CounterComponent(id="A")
-      span.row.justify-center.items-start
-        span.text-h4.text-purple-9.q-my-md +
-        CounterComponent(id="B")
-      span.row.justify-center.items-start
-        span.text-h4.text-purple-9.q-my-md   =
-        CountersTotal
+    div(v-for="n in 5")
+      .row.justify-center.items-start.q-my-xl
+        span.row.flex.flex-center
+          CounterComponent(id="A")
+        span.row.justify-center.items-start
+          span.text-h4.text-purple-9.q-my-md   =
+          CountersTotal
   div(v-else)
     h4.flex.flex-center.text-deep-purple-7.q-mx-xl You are not logged in !
 </template>
