0d144eb6a25d549d3cb445e16569808ea6ac04ad
Tanous Raad <traad@sebia.com>
2022-10-13 13:19:21 +0200

    Prompt example

--- src/components/CreateCounter.vue
+++ src/components/CreateCounter.vue
@@ -1,7 +1,21 @@
-<script setup>
+<script>
 import { defineComponent } from "vue";
 
-defineComponent({ name: "CreateCounter" });
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    defineComponent({ name: "CreateCounter" });
+
+    return {
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+
+      address: ref('')
+    }
+  }
+}
 </script>
 
 <template lang="pug">
@@ -28,7 +42,18 @@ defineComponent({ name: "CreateCounter" });
         size="0.9em",
         icon="sync",
         label="Pull shared counters",
-        @click = "syncFromServer"
+        @click = "prompt = true"
         )
         q-tooltip(anchor="bottom right").bg-teal Pull shared counters
+        q-dialog(v-model='prompt' persistent='')
+          q-card(style='min-width: 350px')
+            q-card-section
+              .text-h6 Your address
+            q-card-section.q-pt-none
+              q-input(dense='' v-model='address' autofocus='' @keyup.enter='prompt = false')
+            q-card-actions.text-primary(align='right')
+              q-btn(flat='' label='Cancel' v-close-popup='')
+              q-btn(flat='' label='Add address' v-close-popup='')
+
+
 </template>
