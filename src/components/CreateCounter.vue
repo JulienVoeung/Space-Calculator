96e0b3a89736afdf4315995b99592118e3af08a6
Lionel RAKOTOARISOA <rakotoarisoa@et.esiea.fr>
2022-10-13 13:21:50 +0200

    prompt try

--- src/components/CreateCounter.vue
+++ src/components/CreateCounter.vue
@@ -16,8 +16,17 @@ defineComponent({ name: "CreateCounter" });
         size="0.9em",
         icon="add",
         label="Create a new counter",
-        @click = "syncToServer"
+        @click = "prompt = true"
         )
+        q-dialog(v-model="prompt" persistent='')
+          q-card(style='min-width: 350px')
+            q-card-section
+              .text-h6 counter name
+            q-card-section.q-pt-none
+              q-input(dense='', v-model="address", autofocus='' @keyup.enter="prompt = false")
+            q-card-actions.text-primary(align='right')
+              q-btn(label='Cancel', @click="prompt = false")
+              q-btn(label='Add button')
         q-tooltip(anchor="bottom left").bg-teal Create a new counter
     .column.col-5
       q-btn.q-ma-xs(
@@ -28,7 +37,7 @@ defineComponent({ name: "CreateCounter" });
         size="0.9em",
         icon="sync",
         label="Pull shared counters",
-        @click = "syncFromServer"
+        @click = ""
         )
         q-tooltip(anchor="bottom right").bg-teal Pull shared counters
 </template>
