b05365ab945b0f1240c59e339cae417c61aac789
Lionel RAKOTOARISOA <rakotoarisoa@et.esiea.fr>
2022-10-23 09:55:30 +0200

    added test for import counter

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,10 +8,10 @@ describe("E2E SPEC", () => {
     cy.get(".q-tab__label").contains("Sign In").click();
     cy.get(".q-field__control-container")
       .find('[type="Email"]')
-      .type("lionelrakotoarisoa@gmail.com");
+      .type("nenage1064@24rumen.com");
     cy.get(".q-field__control-container")
       .find('[type="Password"]')
-      .type("prout69latrik");
+      .type("testtest");
     cy.get(".full-width").find('[type="submit"]').click();
     cy.get(".newcounter").find("button").click();
     cy.get(".q-field__control").find("input").type("A");
@@ -19,15 +19,33 @@ describe("E2E SPEC", () => {
     for (let i = 0; i < max; i++) {
       cy.get(".increment").find("button").click();
     }
-    cy.get(".q-field__control-container")
-      .find("input")
-      .should("have.value", max);
+    cy.get(".counternumber").find("input").should("have.value", max);
     cy.get(".save").find("button").click();
-    cy.get(".q-field__control-container").find("button").click();
-    cy.get(".q-field__control-container").find("input").should("have.value", 0);
+    cy.get(".counternumber").find("button").click();
+    cy.get(".counternumber").find("input").should("have.value", 0);
     cy.get(".load").find("button").click();
+    cy.get(".counternumber").find("input").should("have.value", max);
+    cy.get(".deletecounter").find("button").click();
+  });
+
+  it("correctly import a counter", () => {
+    const max = Math.floor(Math.random() * 19 + 1);
+    cy.get(".q-tab__label").contains("Sign In").click();
     cy.get(".q-field__control-container")
+      .find('[type="Email"]')
+      .type("nenage1064@24rumen.com");
+    cy.get(".q-field__control-container")
+      .find('[type="Password"]')
+      .type("testtest");
+    cy.get(".full-width").find('[type="submit"]').click();
+    cy.get(".import").find("button").click();
+    cy.get(".inputcounterid")
       .find("input")
-      .should("have.value", max);
+      .type("484596c2-ea07-44b3-8c77-302fa678a26d");
+    cy.get(".validatecounterid").find("button").click();
+    cy.get(".counternumber").find("input").should("have.value", 12);
   });
 });
+
+/* nenage1064@24rumen.com:testtest */
+/* caladil264@24rumen.com:test2test2 */
