8c4fe8a75b960e789e64c6ce9cb73e42a3910277
voeung <voeung@et.esiea.fr>
2022-10-23 15:56:19 +0200

    Update test responsiveness

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,15 +3,14 @@ describe("E2E SPEC", () => {
     cy.visit("http://localhost:9000");
   });
 
-// WARNING: ALL COUNTERS FROM TEST ACCOUNTS MUST BE DELETED BEFORE TO RUN TESTS
+  // WARNING: ALL COUNTERS FROM TEST ACCOUNTS MUST BE DELETED BEFORE TO RUN TESTS
 
-/* nenage1064@24rumen.com:testtest */
-/* caladil264@24rumen.com:test2test2 */
+  /* nenage1064@24rumen.com:testtest */
+  /* caladil264@24rumen.com:test2test2 */
 
   it("correctly adds a functional counter and test responsiveness", () => {
     const max = Math.floor(Math.random() * 19 + 1);
     cy.get(".q-tab__label").contains("Sign In").click();
-    cy.get(".q-tab__label").contains("Sign In").should("be.visible");
     cy.get(".q-field__control-container")
       .find('[type="Email"]')
       .type("nenage1064@24rumen.com");
@@ -22,7 +21,6 @@ describe("E2E SPEC", () => {
     cy.get('[data-cy="newCounter"]').click();
     cy.get('[data-cy="newCounterInput"]').type("A");
     cy.get('[data-cy="newCounterInputValidate"]').click();
-    cy.get('[data-cy="newCounterInputValidate"]').should("be.visible");
     for (let i = 0; i < max; i++) {
       cy.get('[data-cy="increment"]').click();
     }
@@ -34,15 +32,20 @@ describe("E2E SPEC", () => {
     cy.get('[data-cy="input"]').should("have.value", max);
 
     //begin the responsiveness test
-    cy.get('[data-cy="newCounter"]').should("be.visible");
-    cy.get('[data-cy="newCounter"]').click();
-    cy.get('[data-cy="newCounterInput"]').type("A").should("be.visible");
-    cy.get('[data-cy="newCounterInputValidate"]').should("be.visible");
-    cy.get('[data-cy="newCounterInputValidate"]').click();
-    cy.get('[data-cy="saveToLocal"]').should("be.visible");
-    cy.get('[data-cy="loadFromLocal"]').should("be.visible");
-    cy.get('[data-cy="input"]').should("be.visible");
-    cy.get('[data-cy="deleteCounter"]').should("be.visible");
+    const platforms = ["ipad-mini", "iphone-x", "macbook-16", "samsung-s10"];
+    platforms.forEach((platform) => {
+      cy.viewport(`${platform}`);
+      cy.get('[data-cy="newCounter"]').should("be.visible");
+      cy.get('[data-cy="newCounter"]').click();
+      cy.get('[data-cy="newCounterInputValidate"]').should("be.visible");
+      cy.get('[data-cy="newCounterInput"]').type("A").should("be.visible");
+      cy.get('[data-cy="newCounterInputValidate"]').should("be.visible");
+      cy.get('[data-cy="newCounterInputValidate"]').click();
+      cy.get('[data-cy="saveToLocal"]').should("be.visible");
+      cy.get('[data-cy="loadFromLocal"]').should("be.visible");
+      cy.get('[data-cy="input"]').should("be.visible");
+      cy.get('[data-cy="deleteCounter"]').should("be.visible");
+    });
     //end the responsiveness test
 
     cy.get('[data-cy="deleteCounter"]').click();
@@ -59,7 +62,9 @@ describe("E2E SPEC", () => {
       .type("testtest");
     cy.get(".full-width").find('[type="submit"]').click();
     cy.get('[data-cy="importCounter"]').click();
-    cy.get('[data-cy="importCounterInput"]').type("484596c2-ea07-44b3-8c77-302fa678a26d");
+    cy.get('[data-cy="importCounterInput"]').type(
+      "484596c2-ea07-44b3-8c77-302fa678a26d"
+    );
     cy.get('[data-cy="importCounterInputValidate"]').click();
     cy.get('[data-cy="input"]').should("have.value", 12);
 
